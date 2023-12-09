b21a102531343b025e561e60b6befeba3a210a8e
iliasen <iliasen03@mail.ru>
2023-12-09 10:36:42 +0300

    Creating api with DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,29 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
-    id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    kotlin("jvm") version "1.9.10" apply false
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    kotlin("plugin.serialization") version "1.9.10"
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schema")
+    arg("room.incremental", "true")
+    arg("room.expandProjection", "true")
 }
 
 android {
-    namespace = "com.iliasen.lab1n"
+    namespace = "by.bsuir.vitaliybaranov.myapplication"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.iliasen.lab1n"
+        applicationId = "by.bsuir.vitaliybaranov.myapplication"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+        compileSdkPreview = "UpsideDownCake"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -26,64 +34,89 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
+        dataBinding = true
+        viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    applicationVariants.all {
+        addJavaSourceFoldersToModel(
+            File(buildDir, "generated/ksp/$name/kotlin")
+        )
+    }
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.versions.androidx.compose.material)
+    implementation(libs.androidx.androidx.room.gradle.plugin)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
 
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 
-    implementation("androidx.datastore:datastore:1.0.0")
+    implementation(libs.androidx.ui.text)
+    implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.compose.destinations.ksp)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.android)
+    implementation(libs.koin.core)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.ktor)
 
-    implementation ("io.insert-koin:koin-androidx-compose:3.4.3")
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.serialization)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation(libs.androidx.monitor)
 
-    implementation ("androidx.work:work-runtime-ktx:2.8.1")
-
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
-
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-
-    implementation ("androidx.paging:paging-runtime-ktx:3.1.1")
-
-    implementation ("io.ktor:ktor-client-core:2.3.4")
-    implementation ("io.ktor:ktor-client-android:2.3.4")
+    implementation(libs.bundles.serialization)
+    implementation(libs.kotlin.coroutines)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.okhttp.logging)
+    implementation(libs.paging.compose)
+    implementation(libs.paging.common)
+    implementation(libs.work)
+    implementation(libs.datastore)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.collection)
+    implementation(libs.compose.ui.util)
+    implementation(libs.compose.destinations)
+    debugImplementation(libs.compose.tooling)
 }
+
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
-buildscript {
-
-    dependencies {
-        classpath ("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    kotlin("jvm") version "1.9.10" apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    kotlin("plugin.serialization") version "1.9.10"
+}
+
+buildscript {
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.9.10"))
+    }
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
